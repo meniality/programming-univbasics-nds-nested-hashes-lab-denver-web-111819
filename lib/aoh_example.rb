76a45eb820d7d316bb7db60eb90eb65642825754
patrick hoehn <patrh_99@hotmail.com>
2019-11-08 21:36:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,12 @@ end
 
 def literal_aoh
   aoh = [
-    ]
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
